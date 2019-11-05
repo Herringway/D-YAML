6c5abc628e642ba338be4d18538631afbe0164e8
Cameron Ross <elpenguino@gmail.com>
2019-11-04 21:55:51 -0400

    reimplement Queue on top of DList

--- source/dyaml/queue.d
+++ source/dyaml/queue.d
@@ -6,8 +6,9 @@
 
 module dyaml.queue;
 
+import std.container.dlist;
 
-import std.traits : hasMember, hasIndirections;
+import std.traits : hasMember;
 
 package:
 
@@ -16,141 +17,29 @@ package:
 /// Needed in some D:YAML code that needs a queue-like structure without too much
 /// reallocation that goes with an array.
 ///
-/// Allocations are non-GC and are damped by a free-list based on the nodes
-/// that are removed. Note that elements lifetime must be managed
-/// outside.
 struct Queue(T)
 if (!hasMember!(T, "__xdtor"))
 {
 
 private:
-
-    // Linked list node containing one element and pointer to the next node.
-    struct Node
-    {
-        T payload_;
-        Node* next_;
-    }
-
-    // Start of the linked list - first element added in time (end of the queue).
-    Node* first_;
-    // Last element of the linked list - last element added in time (start of the queue).
-    Node* last_;
-    // free-list
-    Node* stock;
-
-    // Length of the queue.
+    //// Length of the queue.
     size_t length_;
-
-    // allocate a new node or recycle one from the stock.
-    Node* makeNewNode(T thePayload, Node* theNext = null) @trusted nothrow @nogc
-    {
-        import std.experimental.allocator : make;
-        import std.experimental.allocator.mallocator : Mallocator;
-
-        Node* result;
-        if (stock !is null)
-        {
-            result = stock;
-            stock = result.next_;
-            result.payload_ = thePayload;
-            result.next_ = theNext;
-        }
-        else
-        {
-            result = Mallocator.instance.make!(Node)(thePayload, theNext);
-            // GC can dispose T managed member if it thinks they are no used...
-            static if (hasIndirections!T)
-            {
-                import core.memory : GC;
-                GC.addRange(result, Node.sizeof);
-            }
-        }
-        return result;
-    }
-
-    // free the stock of available free nodes.
-    void freeStock() @trusted @nogc nothrow
-    {
-        import std.experimental.allocator.mallocator : Mallocator;
-
-        while (stock !is null)
-        {
-            Node* toFree = stock;
-            stock = stock.next_;
-            static if (hasIndirections!T)
-            {
-                import core.memory : GC;
-                GC.removeRange(toFree);
-            }
-            Mallocator.instance.deallocate((cast(ubyte*) toFree)[0 .. Node.sizeof]);
-        }
-    }
+    //// Doubly-linked list used as queue
+    DList!T list;
 
 public:
 
-    @disable void opAssign(ref Queue);
-    @disable bool opEquals(ref Queue);
-    @disable int opCmp(ref Queue);
-
-    this(this) @safe nothrow @nogc
-    {
-        auto node = first_;
-        first_ = null;
-        last_ = null;
-        while (node !is null)
-        {
-            Node* newLast = makeNewNode(node.payload_);
-            if (last_ !is null)
-                last_.next_ = newLast;
-            if (first_ is null)
-                first_      = newLast;
-            last_ = newLast;
-            node = node.next_;
-        }
-    }
-
-    ~this() @safe nothrow @nogc
-    {
-        freeStock();
-        stock = first_;
-        freeStock();
-    }
-
     /// Returns a forward range iterating over this queue.
-    auto range() @safe pure nothrow @nogc
+    auto range() @safe pure nothrow
     {
-        static struct Result
-        {
-            private Node* cursor;
-
-            void popFront() @safe pure nothrow @nogc
-            {
-                cursor = cursor.next_;
-            }
-            ref T front() @safe pure nothrow @nogc
-            in(cursor !is null)
-            {
-                return cursor.payload_;
-            }
-            bool empty() @safe pure nothrow @nogc const
-            {
-                return cursor is null;
-            }
-        }
-        return Result(first_);
+        return list;
     }
 
     /// Push a new item to the queue.
-    void push(T item) @nogc @safe nothrow
+    void push(T item) @safe nothrow
     {
-        Node* newLast = makeNewNode(item);
-        if (last_ !is null)
-            last_.next_ = newLast;
-        if (first_ is null)
-            first_      = newLast;
-        last_ = newLast;
-        ++length_;
+        list.insertFront(item);
+        length_++;
     }
 
     /// Insert a new item putting it to specified index in the linked list.
@@ -161,85 +50,58 @@ public:
     }
     do
     {
-        if (idx == 0)
+        if (idx == length_)
         {
-            first_ = makeNewNode(item, first_);
-            ++length_;
-        }
-        // Adding before last added element, so we can just push.
-        else if (idx == length_)
+            list.insertFront(item);
+        } else
         {
-            push(item);
+            auto x = list[];
+            foreach (i; 0..idx) {
+                x.popBack();
+            }
+            list.insertAfter(x, item);
         }
-        else
-        {
-            // Get the element before one we're inserting.
-            Node* current = first_;
-            foreach (i; 1 .. idx)
-                current = current.next_;
-
-            assert(current);
-            // Insert a new node after current, and put current.next_ behind it.
-            current.next_ = makeNewNode(item, current.next_);
-            ++length_;
-        }
+        length_++;
     }
 
     /// Returns: The next element in the queue and remove it.
-    T pop() @safe nothrow
+    T pop()
     in
     {
         assert(!empty, "Trying to pop an element from an empty queue");
     }
     do
     {
-        T result = peek();
-
-        Node* oldStock = stock;
-        Node* old = first_;
-        first_ = first_.next_;
-
-        // start the stock from the popped element
-        stock = old;
-        old.next_ = null;
-        // add the existing "old" stock to the new first stock element
-        if (oldStock !is null)
-            stock.next_ = oldStock;
-
-        if (--length_ == 0)
-        {
-            assert(first_ is null);
-            last_ = null;
-        }
-
-        return result;
+        length_--;
+        scope(exit) list.removeBack();
+        return list.back;
     }
 
     /// Returns: The next element in the queue.
-    ref inout(T) peek() @safe pure nothrow inout @nogc
+    ref inout(T) peek() @safe pure nothrow inout
     in
     {
         assert(!empty, "Trying to peek at an element in an empty queue");
     }
     do
     {
-        return first_.payload_;
+        return list.back;
     }
 
     /// Returns: true of the queue empty, false otherwise.
-    bool empty() @safe pure nothrow const @nogc
+    bool empty() @safe pure nothrow const
     {
-        return first_ is null;
+        return list.empty;
     }
 
     /// Returns: The number of elements in the queue.
-    size_t length() @safe pure nothrow const @nogc
+    size_t length() @safe pure nothrow const
     {
         return length_;
     }
 }
 
-@safe nothrow unittest
+@safe pure nothrow unittest
 {
     auto queue = Queue!int();
     assert(queue.empty);
